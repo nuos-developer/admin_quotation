575517ee6be9337bbdc2148713c3e8192e180192
nuos-developer <developer@nuos.in>
2026-02-11 15:44:49 +0530

    env file change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation
+} from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideRouter(
+      routes,
+      withEnabledBlockingInitialNavigation()
+    ),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
+// local IP
 export const environment = {
   apiUrl: 'http://192.168.1.14:8080/api'
 };
 
-//localhttp://localhost:8080
+// production Ip
+// export const environment = {
+//   apiUrl: 'http://16.171.67.6:8001/api'
+// };
+
